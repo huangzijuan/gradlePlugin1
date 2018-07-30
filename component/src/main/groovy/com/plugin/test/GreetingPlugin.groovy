809399458be013826f3c33fe429c5fcdbfc90dae
huangzijuan <1255879426@qq.com>
2018-07-30 21:37:27 +0800

    gradle插件提交到本地

---
+++ component/src/main/groovy/com/plugin/test/GreetingPlugin.groovy
@@ -0,0 +1,14 @@
+package com.plugin.test
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GreetingPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('hello') {
+            doLast {
+                println 'Hello from the GreetingPlugin: hhhhhhhhhhhhhhh'
+            }
+        }
+    }
+}
